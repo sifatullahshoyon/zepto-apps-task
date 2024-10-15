22561fab6143b777ce851cea439555ed8ce7ac62
freelancersifat627e <freelancersifat627@gmail.comm>
2024-10-15 17:35:56 +0600

    compleate navbar with fully responsive

---
+++ script/index.js
@@ -0,0 +1,14 @@
+// Start Navbar Code
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'inline'
+};
+    
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+};
+
+
+// End Navbar Code
